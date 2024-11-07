7df7b1b0f143fad72704d41edf217c74be1618c0
MelisaOksum <melisa_oksum@hotmail.com>
2024-11-07 11:08:12 +0100

    billede på scoreside

--- src/components/ScoreComponent.vue
+++ src/components/ScoreComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container>
-  <h1 class="score-title">Dagens score</h2>
-    
+  <h1 class="score-title">Dagens score</h1>
+
     <div class="score-value">400</div>
 
     <v-card-text>
@@ -9,24 +9,47 @@
     </v-card-text>
     <p class="co2-amount">1000 gram CO₂ i dag</p>
 
-    <div class="tree-section">
-      <p>Vand dit træ og få dagens point</p>
-      <img src="images/fa-solid_award.svg" alt="træ" class="award-icon" />
-    </div>
+    <p>Vand dit træ og få dagens point</p>
+    <v-img
+    :width="300"
+    aspect-ratio="16/9"
+    cover
+    src="images/fa-solid_award.svg">
+    </v-img>
 
     <div class="carbon-reduction">
       <strong>REDUCERE 600KG CARBON</strong> per uge
     </div>
     <div class="button-group">
-      <button @click="goBack" class="back-button">Tilbage</button>
-      <button @click="goHome" class="home-button">Startside</button>
+      <v-btn @click="goBack" class="back-button">Tilbage</v-btn>
+      <v-btn @click="goHome" class="home-button">Startside</v-btn>
     </div>
-  </div>
-
-
-
-
 
 
   </v-container>
 </template>
+
+
+<script>
+import { useRouter } from 'vue-router';
+
+export default {
+  name: 'ScoreComponent',
+  setup() {
+    const router = useRouter();
+
+    const goBack = () => {
+      router.push('#');
+    };
+
+    const goHome = () => {
+      router.push('#');
+    };
+
+    return {
+      goBack,
+      goHome
+    };
+  }
+};
+</script>
