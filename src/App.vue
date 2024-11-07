da1e59a9a409afd98d652b5737e2373b85e6a03a
YaMe09 <134693503+YaMe09@users.noreply.github.com>
2024-11-07 21:07:19 +0100

    App style

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
     <v-container v-if="showWelcome" class="forside" fill-height>
       <v-row align="center" justify="center">
         <v-col>
-          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
           <h1 :style="{ fontWeight: '700' }">Velkommen</h1>
           <br />
-          <br /><br /><br /><br /><br /><br />
+          <br /><br /><br /><br />
           <p :style="{ fontWeight: '700' }">Gør noget for at passe på fremtiden</p>
           <br /><br /><br /><br /><br /><br /><br />
           <AppknappComponent @navigate="hideWelcome" />
@@ -50,7 +50,7 @@ export default defineComponent({
   background-image: url('./images/forside.svg');
   background-size: cover;
   background-position: center;
-  margin-top: 50px;
+  margin-top: 60px;
 }
 
 h1, p {
